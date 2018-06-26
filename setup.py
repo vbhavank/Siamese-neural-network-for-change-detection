ceb7ef9224f09c96ff27352d0e7324277888aff3
Bhavan <33493110+vbhavank@users.noreply.github.com>
2018-06-26 12:12:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="siamese_change",
+    name="siamese_rit_change",
     version="0.0.1",
     author="Bhavan Vasu, Faiz Ur Rahman, Andreas Savakis",
     author_email="bxv7657@rit.edu",
     description="A package code for aerial patch based change detection",
     long_description=long_description,
-    long_description_content_type="We present a patch-based algorithm for detecting structural changes in satellite imagery using a Siamese neural network. The two channels of our Siamese network are based on the VGG16 architecture with shared weights. Changes between the target and reference images are detected with a fully connected decision network that was trained on DIRSIG simulated samples and achieved a high detection rate. Alternatively, a change detection approach based on Euclidean distance between deep convolutional features achieved very good results with minimal supervision.",
+    long_description_content_type="text/markdown",
     url="https://github.com/vbhavank/Siamese-neural-network-for-change-detection",
     packages=setuptools.find_packages(),
     classifiers=(
